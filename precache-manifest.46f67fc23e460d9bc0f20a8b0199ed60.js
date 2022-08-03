74e7698761d0eef53083cf113139212962bc141e
lucaseduardolema <lucaseduardolema@users.noreply.github.com>
2022-08-03 02:42:12 +0000

    deploy: 984ff6603201715efae16164d325cf3d73c6cab3

---
+++ precache-manifest.46f67fc23e460d9bc0f20a8b0199ed60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe7cf8c5b91b13bb026c5652e278871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a54c8b0361ec2248fc1b",
+    "url": "/static/css/main.f61bf671.chunk.css"
+  },
+  {
+    "revision": "e07cb39e7bd7f5b405ff",
+    "url": "/static/js/2.52be3c6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.52be3c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54c8b0361ec2248fc1b",
+    "url": "/static/js/main.38853d23.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.38853d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  }
+]);
